c2c34c207624a88ad3de8025aa85a0a465909133
Santiago Pourteau <spourteau@udesa.edu.ar>
2024-12-09 11:10:02 -0300

    png

--- Desarrollo/scripts/modify_dataset/to_4_channels.py
+++ Desarrollo/scripts/modify_dataset/to_4_channels.py
@@ -6,10 +6,13 @@ import numpy as np
 import torch 
 
 path = '../../data_yolo/data_renamed/'
-
 #create folder if not exists
-if not os.path.exists('../../data_yolo/dataset_4channels'):
-    os.makedirs('../../data_yolo/dataset_4channels')
+new_file_type = "png"
+# new_file_type = "pt"
+# new_file_type = "npy"
+new_path = '../../data_yolo/dataset_4channels_'+new_file_type
+if not os.path.exists(new_path):
+    os.makedirs(new_path)
     
 for animal in os.listdir(path):
     if os.path.isdir(path + animal):
@@ -24,11 +27,22 @@ for animal in os.listdir(path):
                             
                             combined = cv2.merge((rgb, thermal))
                             # write in a new folder called dataset_4channels
-                            if not os.path.exists('../../data_yolo/dataset_4channels/images'):
-                                os.makedirs('../../data_yolo/dataset_4channels/images')
+                            if not os.path.exists(new_path + '/images'):
+                                os.makedirs(new_path + '/images')
                             file = file.replace('RGB', '4channels')
-                            transform = torch.from_numpy(combined)
-                            torch.save(transform, '../../data_yolo/dataset_4channels/images/' + file + '.pt')
+                            if new_file_type == "pt":
+                                transform = torch.from_numpy(combined)
+                                torch.save(transform, new_path + '/images/' + file)
+                            elif new_file_type == "png":
+                                #save as a png with 4 channels imwrite
+                                png_image = cv2.imwrite(new_path + '/images/' + file.replace('jpg', 'png'), combined)
+                                # png_image = cv2.imwrite(new_path + '/images/' + file, combined)
+                            elif new_file_type == "npy":
+                                np.save(new_path + '/images/' + file, combined)
+                            else:
+                                print("Invalid file type")
+
+                                
 
                 elif subfolder == 'labels':
                     for file in os.listdir(path + animal + '/' + scene + '/' + subfolder):
@@ -36,10 +50,10 @@ for animal in os.listdir(path):
                             with open(path + animal + '/' + scene + '/' + subfolder + '/' + file, 'r') as f:
                                 lines = f.readlines()
                             #create file if exists
-                            if not os.path.exists('../../data_yolo/dataset_4channels/labels'):
-                                os.makedirs('../../data_yolo/dataset_4channels/labels')
+                            if not os.path.exists(new_path + '/labels'):
+                                os.makedirs(new_path + '/labels')
                             file = file.replace('RGB', '4channels')
-                            with open('../../data_yolo/dataset_4channels/labels/' + file, 'w') as f:
+                            with open(new_path + '/labels/' + file, 'w') as f:
                                 for line in lines:
                                     f.write(line)
                         else:
